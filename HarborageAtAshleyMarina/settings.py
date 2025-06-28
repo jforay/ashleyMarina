8461701690454754c7b6c6c0e9af9a521995a4a1
Joe Foray <you@example.com>
2025-06-28 14:50:31 -0400

    point to supabsae postgress in production

--- HarborageAtAshleyMarina/settings.py
+++ HarborageAtAshleyMarina/settings.py
@@ -82,10 +82,11 @@ WSGI_APPLICATION = 'HarborageAtAshleyMarina.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgres://localhost',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
